3e1c3192747457203715ac80310216984a472a71
jcbt <tevesjhunel01@gmail.com>
2025-11-26 17:11:04 +0800

    guardian

--- backend/build.sh
+++ backend/build.sh
@@ -4,7 +4,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py migrations --noinput
+python manage.py makemigrations --noinput
 # Try the standard createsuperuser first (keeps Render's default behavior).
 # If it fails (for example because the user already exists), run the idempotent
 # script which handles existing users gracefully.
